217eed23d5c5a9f3bcf46e3eb54eb9952bfabc3a
Afeez Kazeem <sirdemight@gmail.com>
2025-07-18 09:22:13 +0100

    Second commit

--- weather.py
+++ weather.py
@@ -1,9 +1,14 @@
-temperature = 20
+temperature = 55
+raining = True 
+
 if temperature > 80:
     print("It's too hot")
     print("stay inside!")
 elif temperature < 27:
     print("It's too cold")
+    print("stay inside!")
+elif raining and temperature < 60:
+    print("It's raining")
     print("stay inside!")
 else:
     print("Enjoy the outdoors!")
